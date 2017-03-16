f862d1d97f53db69d8360e7d7eb1a023488fe806
Safiullah Hamidi <safiullah@sahusoft.com>
2017-03-16 17:24:08 +0530

    trial

--- example/App.js
+++ example/App.js
@@ -10,7 +10,7 @@ import moment from 'moment';
 
 const customDayHeadings = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 const customMonthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May',
-  'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 const styles = StyleSheet.create({
   container: {
@@ -38,30 +38,47 @@ class App extends Component {
     };
   }
 
+  onDateSelect(date) {
+    this.setState({selectedDate: date})
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Calendar
-          ref="calendar"
-          eventDates={['2016-07-03', '2016-07-05', '2016-07-28', '2016-07-30']}
-          events={[{date: '2016-07-04', hasEventCircle: {backgroundColor: 'powderblue'}}]}
-          scrollEnabled
-          showControls
-          dayHeadings={customDayHeadings}
-          monthNames={customMonthNames}
-          titleFormat={'MMMM YYYY'}
-          prevButtonText={'Prev'}
-          nextButtonText={'Next'}
-          onDateSelect={(date) => this.setState({ selectedDate: date })}
-          onTouchPrev={(e) => console.log('onTouchPrev: ', e)}
-          onTouchNext={(e) => console.log('onTouchNext: ', e)}
-          onSwipePrev={(e) => console.log('onSwipePrev: ', e)}
-          onSwipeNext={(e) => console.log('onSwipeNext', e)}
-        />
-        <Text>Selected Date: {moment(this.state.selectedDate).format('MMMM DD YYYY')}</Text>
+      <Calendar
+      ref="calendar"
+      customStyle={{calendarContainer:{backgroundColor:'white'},
+                    calendarControls:{backgroundColor:'#0CA0EC'},
+                    calendarHeading:{borderBottomWidth:0},
+                    day: {fontSize: 15, textAlign: 'center',borderWidth:0},
+                    title:{color:'white'},
+                    weekendDayText:{color:'black'},
+                    weekendHeading:{color:'black'},
+                    currentDayText:{color:'black'},
+                    hasEventCircle:{backgroundColor:'red'},
+                    eventIndicator:{backgroundColor:'#35C7A6',height:5,width:5,borderBottomLeftRadius:5,borderBottomRightRadius:5,borderTopLeftRadius:5,borderTopRightRadius:5},
+                  }} // Customize any pre-defined styles
+      dayHeadings={['S', 'M', 'T', 'W', 'T', 'F', 'S']}           // Default: ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+      eventDates={['2017-03-01']}       // Optional array of moment() parseable dates that will show an event indicator
+      events={[{date:'2017-03-02'}]}// Optional array of event objects with a date property and custom styles for the event indicator
+      monthNames={['January','February','March','April','May','June','July','August','September','October','November','Deecmber ']}
+           // Text for next button. Default: 'Next'
+      onDateSelect={(date) => this.onDateSelect(date)} // Callback after date selection
+      onSwipeNext={this.onSwipeNext}    // Callback for forward swipe event
+      onSwipePrev={this.onSwipePrev}    // Callback for back swipe event
+      onTouchNext={this.onTouchNext}    // Callback for next touch event
+      onTouchPrev={this.onTouchPrev}    // Callback for prev touch event
+      
+      scrollEnabled={true}              // False disables swiping. Default: False
+      showControls={true}               // False hides prev/next buttons. Default: False
+      showEventIndicators={true}        // False hides event indicators. Default:False
+      titleFormat={'MM YYYY'}         // Format for displaying current month. Default: 'MMMM YYYY'
+      weekStart={0} // Day on which week starts 0 - Sunday, 1 - Monday, 2 - Tuesday, etc, Default: 1
+      />
+      <Text>Selected Date: {moment(this.state.selectedDate).format('MMMM DD YYYY')}</Text>
       </View>
 
-    );
+      );
   }
 }
 
