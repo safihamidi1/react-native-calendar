cc06d6f6caa13b875719b438425c6beaabab591d
Safiullah Hamidi <safiullah@sahusoft.com>
2017-04-04 04:33:40 +0530

    Adding multiple range select

--- example/App.js
+++ example/App.js
@@ -35,8 +35,8 @@ class App extends Component {
     super(props);
     this.state = {
       selectedDate: moment().format(),
-      leaveStartDate: moment('2017-04-01','YYYY-MM-DD').format(),
-      leaveEndDate: moment('2017-04-05','YYYY-MM-DD').format(),
+      leaveStartDate: ['2017-04-01','2017-04-25'],
+      leaveEndDate: ['2017-04-04','2017-04-28'],
     };
   }
 
@@ -70,8 +70,8 @@ class App extends Component {
       onSwipePrev={this.onSwipePrev}    // Callback for back swipe event
       onTouchNext={this.onTouchNext}    // Callback for next touch event
       onTouchPrev={this.onTouchPrev}    // Callback for prev touch event
-      leaveStartDate={this.state.leaveStartDate}
-      leaveEndDate={this.state.leaveEndDate}
+      leaveStartDate={this.state.leaveStartDate[]}
+      leaveEndDate={this.state.leaveEndDate[]}
       scrollEnabled={true}              // False disables swiping. Default: False
       showControls={true}               // False hides prev/next buttons. Default: False
       showEventIndicators={true}        // False hides event indicators. Default:False
